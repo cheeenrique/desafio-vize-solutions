338e4dbab75e79e1fb8e252f0bf73f73a04e763a
Carlos Henrique <91711053+cheeenrique@users.noreply.github.com>
2022-05-12 10:45:45 -0300

    Atualização de code review no componente header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -43,30 +43,6 @@ export default function Header() {
             <img src={HeaderLogo} alt="Logo" />
           </Link>
         </Logo>
-        {/* <Button
-            id="basic-button"
-            aria-controls={open ? 'basic-menu' : undefined}
-            aria-haspopup="true"
-            aria-expanded={open ? 'true' : undefined}
-            onClick={handleClick}
-            color="info"
-        >
-            <MenuIcon/>
-        </Button>
-        <Menu
-            id="basic-menu"
-            anchorEl={anchorEl}
-            open={open}
-            onClose={handleClose}
-            MenuListProps={{
-            'aria-labelledby': 'basic-button',
-            }}
-        >
-            <MenuItem onClick={handleClose}>Profile</MenuItem>
-            <MenuItem onClick={handleClose}>My account</MenuItem>
-            <MenuItem onClick={handleClose}>Logout</MenuItem>
-        </Menu> */}
-
         <Box
           sx={{ display: 'flex', alignItems: 'center', textAlign: 'center' }}
         >
